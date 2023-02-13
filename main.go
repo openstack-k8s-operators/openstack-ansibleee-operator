4d19f960fd816d806d10d3b9a1e0f07793554d38
rabi <ramishra@redhat.com>
2023-02-13 15:34:44 +0530

    Initialize Log in reconciler setup
    Otherwise all logging results in nil pointer derefencing error.

--- main.go
+++ main.go
@@ -81,6 +81,7 @@ func main() {
 	if err = (&controllers.OpenStackAnsibleEEReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
+		Log:    ctrl.Log.WithName("controllers").WithName("OpenStackAnsibleEE"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "OpenStackAnsibleEE")
 		os.Exit(1)
