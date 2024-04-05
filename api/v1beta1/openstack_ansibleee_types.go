92b8ab8e1ff2123e7810994b5bfdc817e6ca6a14
Francesco Pantano <fpantano@redhat.com>
2024-04-05 09:27:43 +0200

    Re-init conditions each reconcile
    This patch follows the same pattern applied to the other operators,
    where we re-init the condition at each reconcile loop. Conditions
    are re-evaluated and updated, keeping the LastTransitionTime for
    those that haven't changed (it avoids the transition from True to
    Unknown to True again).
    In addition, the "observedGeneration" field is introduced, and it
    is used by the openstack-operator to check the IsReady() function
    for a particular CR in case a minor update is triggered.
    All the conditions are evaluated during the main Reconcile loop (
    or the reconcileNormal function in some circumstances), hence the
    main ReadyCondition is updated within the same flow. The defer
    function still updates the Resource and Mirror the condition to the
    top-level CR.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- api/v1beta1/openstack_ansibleee_types.go
+++ api/v1beta1/openstack_ansibleee_types.go
@@ -134,6 +134,12 @@ type OpenStackAnsibleEEStatus struct {
 	// +kubebuilder:default:=Pending
 	// JobStatus status of the executed job (Pending/Running/Succeeded/Failed)
 	JobStatus string `json:"JobStatus,omitempty" optional:"true"`
+
+	// ObservedGeneration - the most recent generation observed for this
+	// service. If the observed generation is less than the spec generation,
+	// then the controller has not processed the latest changes injected by
+	// the opentack-operator in the top-level CR (e.g. the ContainerImage)
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 //+kubebuilder:object:root=true
